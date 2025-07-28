99988490231943bafed72b77cbc78cbb17f73e8e
Ryan Waldorf <ryan.m.waldorf@gmail.com>
2025-07-28 11:47:51 -0400

    import create_topic

--- processors/source.py
+++ processors/source.py
@@ -35,7 +35,7 @@ from pathlib import Path
 from typing import Dict, Any, Optional
 
 # Import shared functions
-from .common import load_json_file, create_kafka_connection, check_atlas_auth_with_login, create_mongodb_connection, validate_main_config, create_stream_processor
+from .common import load_json_file, create_kafka_connection, check_atlas_auth_with_login, create_mongodb_connection, validate_main_config, create_stream_processor, create_topic
 from .config_validator import validate_connector_config
 
 
